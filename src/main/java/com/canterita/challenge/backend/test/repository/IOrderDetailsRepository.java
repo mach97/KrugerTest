d4708ef0764937087374de0393b5f604394ed50b
Mauro Clavijo <maroclavijoh@yahoo.com>
2021-07-17 01:04:40 -0500

    Finished Part 1

--- src/main/java/com/canterita/challenge/backend/test/repository/IOrderDetailsRepository.java
+++ src/main/java/com/canterita/challenge/backend/test/repository/IOrderDetailsRepository.java
@@ -2,9 +2,12 @@ package com.canterita.challenge.backend.test.repository;
 
 import com.canterita.challenge.backend.test.model.OrderDetailsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import java.util.List;
 
-public interface IOrderDetailsRepository extends JpaRepository<OrderDetailsEntity, Long> {
+public interface IOrderDetailsRepository extends JpaRepository<OrderDetailsEntity, Long>, QuerydslPredicateExecutor<OrderDetailsEntity> {
 
 }
--- src/main/java/com/canterita/challenge/backend/test/service/OrderDetailsService.java
+++ src/main/java/com/canterita/challenge/backend/test/service/OrderDetailsService.java
@@ -4,21 +4,35 @@ package com.canterita.challenge.backend.test.service;
 import com.canterita.challenge.backend.test.dto.OrderDetailsDto;
 import com.canterita.challenge.backend.test.dto.request.OrderDetailsRequest;
 import com.canterita.challenge.backend.test.model.OrderDetailsEntity;
+import com.canterita.challenge.backend.test.model.QOrderDetailsEntity;
 import com.canterita.challenge.backend.test.repository.IOrderDetailsRepository;
+import com.querydsl.core.support.FetchableQueryBase;
+import com.querydsl.jpa.impl.JPAQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Service
 public class OrderDetailsService implements IOrderDetailsService{
-	
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
 	@Autowired
 	private IOrderDetailsRepository IOrderDetailsRepository;
 
 	@Override
 	public List<OrderDetailsEntity> getOrderDetails(Long id) {
-		return IOrderDetailsRepository.findAll();
+		JPAQuery query = new JPAQuery(entityManager);
+		System.out.println("Hola");
+		QOrderDetailsEntity detail = QOrderDetailsEntity.orderDetailsEntity;
+		FetchableQueryBase result = (FetchableQueryBase) query.from(detail).where(detail.idOrder.eq(id));
+
+		return result.fetch();
+		//return IOrderDetailsRepository.findAll();
 				//.stream().filter(orderDetailsEntity -> orderDetailsEntity.getIdOrder().equals(id));
 				//.map(order -> new OrderDetailsDto(order.getId(), order.getIdOrder(), order.getDetail(), order.getCantidad(), order.getPrecioUnitario(), order.getTotalDetail()));
 				//.orElse(null);
