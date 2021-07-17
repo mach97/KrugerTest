00867a354039cb965f578c8a681de00a7e7c069b
Mauro Clavijo <maroclavijoh@yahoo.com>
2021-07-16 21:10:00 -0500

    Changed

--- src/main/java/com/canterita/challenge/backend/test/repository/IOrderDetailsRepository.java
+++ src/main/java/com/canterita/challenge/backend/test/repository/IOrderDetailsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IOrderDetailsRepository extends JpaRepository<OrderDetailsEntity, Long> {
-List<OrderDetailsEntity> findByOrderId(Long id);
+
 }
